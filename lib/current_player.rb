eaef3e2b4001cc0d415ef859ecff9451d00910f1
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-03-09 01:46:39 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,9 @@
 def turn_count(board)
-#  board = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
  board.each do |position|
   counter = 0 
     if position == "X" || position == "O"
       counter += 1 
+      
   end
 end
 
